265eb3ae0a10182043ae00fd28ad5da7743cbc04
nimishgo <nimish.2711@gmail.com>
2022-10-09 20:57:15 +0530

    feat: added function to download pdf of resume

--- src/components/CvMain.js
+++ src/components/CvMain.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import {FiMail,FiGithub , FiLinkedin , FiPhone } from 'react-icons/fi'
+import jsPDF from 'jspdf';
+// import {FiMail,FiGithub , FiLinkedin , FiPhone } from 'react-icons/fi'
 const CvMain = ({info,education,experience,project,skills,loadExample,resetIt}) => {
   const {name,email,git,linkedin,phone} = info;
   console.log(project);
+
+  const generatePdf = () => {
+      let doc = new jsPDF('p','px',[1250,1000]);
+      doc.html(document.querySelector('.cvMain'), {
+        callback: function(pdf){
+          let pageCount = doc.internal.getNumberOfPages();
+          pdf.deletePage(pageCount);
+          pdf.save('resume.pdf');
+        }
+      })
+    }
+
   return (
     <>
       <div className="buttons">
             <input type="button" value="load example" onClick={loadExample}/>
-            <input type="button" value="download pdf"/>
+            <input type="button" value="download pdf" onClick={generatePdf}/>
             <input type="button" value="reset" onClick={resetIt}/>
       </div>
       <section className="cvMain">
               <article className='personals' key='personals'>
                 <h1 className='heading'>{name}</h1>
                 <div className="links">
-                <span> <FiMail/> : {email}</span>
-                <span> <FiGithub /> : {git} </span>
-                <span> <FiLinkedin/>  : {linkedin}</span>
-                <span> <FiPhone />  : {phone}</span>
+                <span> Email : {email}</span>
+                <span> Github : {git} </span>
+                <span> Linkedin  : {linkedin}</span>
+                <span> Phone  : {phone}</span>
                 </div>
               </article> 
               <article className="education">
