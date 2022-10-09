20c44a8d70bf3629433991a7d107a43235c19a73
nimishgo <nimish.2711@gmail.com>
2022-10-09 15:11:40 +0530

    feat: added add and remove

--- src/components/Basics.js
+++ src/components/Basics.js
@@ -1,27 +1,119 @@
 import { Component } from "react";
-
 class Basics extends Component {
-  // constructor() {
-  //   super()
-  // }
+
+  constructor(props) {
+    super(props);
+    this.state = {...props};
+    console.log(this.state);
+  }
+
+  renderChange = (e) => {
+      this.setState({
+        info: {
+          name: e.target.value,
+        }
+      })
+      this.state.renderName(e.target.value);
+    }
+
+    renderEmail = (e) => {
+      this.setState({
+        info: {
+          email: e.target.value,
+        }
+      })
+      this.state.renderEmail(e.target.value);
+  }
+
+    renderGit = (e) => {
+      this.setState(
+        {
+          info: {
+            git: e.target.value,
+          }
+        }
+      )
+      this.state.renderGit(e.target.value);
+    }
+
+    renderLinkedin = (e) => {
+      this.setState(
+        {
+          info: {
+            linkedin: e.target.value,
+          }
+        }
+      )
+      this.state.renderLinkedin(e.target.value);
+    }
+
+    renderPhone = (e) => {
+      this.setState(
+        {
+          info: {
+            phone: e.target.value,
+          }
+        }
+      )
+      this.state.renderPhone(e.target.value);
+    }
+
   render() {
+
+    const {info} = this.state;
+    console.log(info);
     return(
       <>
-      <form>
+      <form onSubmit={this.addtoArr}>
       <fieldset className="field_set">
       <legend>Basic Information</legend>
-        <input type="text" name="name" className="name" placeholder="Your full name" />
-        <input type="text" name="name" className="name" placeholder="Email eg. johndoe@gmail.com" />
-        <input type="text" name="name" className="name" placeholder="Github link" />
-        <input type="text" name="name" className="name" placeholder="Linkedin Link" />
-        <input type="text" name="name" className="name" placeholder="Phone number" />
+        <input 
+          onChange={this.renderChange}
+          value={info.name}
+          type="text" 
+          name="name" 
+          className="name" 
+          placeholder="Your full name"
+          />
+        <input
+          onChange={this.renderEmail}
+          value={info.email}
+          type="text" 
+          name="name" 
+          className="name" 
+          placeholder="Email eg. johndoe@gmail.com"
+          />
+        <input
+          onChange={this.renderGit}
+          value={info.git}
+          type="text" 
+          name="name" 
+          className="name" 
+          placeholder="Github link"
+          />
+        <input
+          onChange={this.renderLinkedin}
+          value={info.Linkedin} 
+          type="text" 
+          name="name" 
+          className="name" 
+          placeholder="Linkedin Link" 
+          
+          />
+        <input
+          onChange={this.renderPhone}
+          value={info.phone} 
+          type="text" 
+          name="name" 
+          className="name" 
+          placeholder="Phone number" 
+          />
       </fieldset>
       </form>
     </>
-  )
+  );
   }
-
 }
-// kbd.styles``;
-
+// export let arrOfInfo = [];
+// console.log(arrOfInfo);
 export default Basics;
--- src/components/Education.js
+++ src/components/Education.js
@@ -1,22 +1,133 @@
 import { Component } from "react";
-
+import uniqid from "uniqid";
 class Education extends Component {
-  // constructor() {
-  //   super()
-  // }
+  
+  constructor(props) {
+    super(props);
+    const {renderEducation } = props;
+    console.log(renderEducation);
+    this.state = {
+    education: [{
+      collegeName: '',
+      degree: '',
+      branch: '',
+      city: '',
+      from: '',
+      to: '',
+      cgpa: '',
+      id: uniqid()
+    }],
+    renderEducation
+  };
+    console.log(this.state);
+  }
+
+  deleteEntry = (e) => {
+          console.log(e.target);
+          const newArr = this.state.education.filter((val,index) => (
+              val.id !== e.target.id
+          ));
+
+          this.setState({
+            education: newArr,
+          }) 
+
+          this.state.renderEducation(newArr);
+  };
+
+  onChangeEdu = (e,index) => {
+      console.log(e.target.value);
+      const arr = [...this.state.education];
+      const {name,value} = e.target;
+      console.log(name,value,arr,index);
+      arr[index][name] = value;
+      this.setState(arr);
+      // (this.state.education);
+      this.state.renderEducation(arr);
+    }
+
   render() {
+    const {education} = this.state;
+    console.log(education);
     return(
       <>
       <form>
       <fieldset className="field_set">
       <legend>Education</legend>
-        <input type="text" name="college" className="name" placeholder="College Name" />
-        <input type="text" name="degree" className="name" placeholder="Degree" />
-        <input type="text" name="branch" className="name" placeholder="City Name" />
-        <input type="text" name="branch" className="name" placeholder="Branch" />
-        <input type="text" name="from" className="name" placeholder="From" />
-        <input type="text" name="to" className="name" placeholder="To" />
-        <input type="text" name="cgpa" className="name" placeholder="CGPA" />
+        {education.map((val,index) => (
+          <div key={val.id}>
+
+          <input 
+            type="text" 
+            name="collegeName" 
+            placeholder="College Name" 
+            value={val.collegeName}
+            onChange={(e) => {this.onChangeEdu(e,index)}}
+            />
+          <input 
+            type="text" 
+            name="degree" 
+            placeholder="Degree"
+            onChange={(e) => {this.onChangeEdu(e,index)}}
+            value={val.degree}
+            />
+          <input 
+            type="text" 
+            name="city" 
+            placeholder="City Name" 
+            onChange={(e) => {this.onChangeEdu(e,index)}}
+            value={val.city}
+            />
+          <input 
+            type="text" 
+            name="branch" 
+            placeholder="Branch" 
+            onChange={(e) => {this.onChangeEdu(e,index)}}
+            value={val.branch}
+            />
+          <input 
+            type="text" 
+            name="from" 
+            placeholder="From" 
+            onChange={(e) => {this.onChangeEdu(e,index)}}
+            value={val.from}
+            />
+          <input 
+            type="text" 
+            name="to" 
+            placeholder="To" 
+            onChange={(e) => {this.onChangeEdu(e,index)}}
+            value={val.to}
+            />
+          <input 
+            type="text" 
+            name="cgpa" 
+            placeholder="CGPA" 
+            onChange={(e) => {this.onChangeEdu(e,index)}}
+            value={val.cgpa}
+            />
+          <div className="buttons">
+          { index === education.length - 1 &&
+            <input 
+            type="button" 
+            value="Add"
+            onClick={() => {
+                this.setState(
+                  {
+                    education : [...education,{branch:"",cgpa:"",city: "",collegeName: "",degree: "",from: "",to: "",id:uniqid()}],
+                  }
+                  );
+              }}
+              />
+          }  
+          {
+            index !== education.length - 1 &&
+            <input type="button" value="Remove"  onClick={this.deleteEntry} id={val.id}/>
+          }
+          </div>
+          </div>
+        ))
+        }
       </fieldset>
       </form>
     </>
@@ -24,6 +135,5 @@ class Education extends Component {
   }
 
 }
-// kbd.styles``;
 
 export default Education;
